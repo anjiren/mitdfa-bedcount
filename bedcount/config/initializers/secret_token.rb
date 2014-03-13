7dd3b6dfade2d756f5a27c0dfd56d8c51b6167d1
Anji Ren <aren@mit.edu>
2014-03-13 18:10:59 -0400

    Created bedcount rails app

---
+++ bedcount/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bedcount::Application.config.secret_key_base = 'c6dbbe1bce22c98b397612788243bf480c12acfb070f1bb77837bb15aa61be6d5d0f824decd79f6f8a4dae53918f07efa4555653fde76136c019d69dd7bb5c74'
---
+++ bedcount/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bedcount::Application.config.session_store :cookie_store, key: '_bedcount_session'
